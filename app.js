4119a4c7b233ee5cc3af048a6d1a3e2ebe4d36e1
Allen McIntosh II <allen.mcintoshii@gmail.com>
2016-11-30 15:38:29 -0500

    Partially finished print count

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
     app.controller('MainController', function () {
         var main = this;
         ///////////////////////////////////////////////////
+        this.printCount = true;
         this.ticketsPrinted = false;
+        this.countPrinted = false;
         this.items = [
             {
                 name: 'Regular Shelf Mixed Drink',
@@ -50,12 +52,12 @@
             {
                 name: 'Cigarettes',
                 price: 10,
-                amount: 40
+//                amount: 40
             },
             {
                 name: 'Black n\' Mild',
                 price: 2,
-                amount: 50
+//                amount: 50
             },
 
         ];
@@ -66,7 +68,6 @@
                     var currentTicketNumber = i + 1;
                     htmlString += "<div class='ticket'>" +
                         "<div class='wrapper'>" +
-                            "<img class='ticket-signature' src='ticket-signature.jpg' />" +
                             "<p class='ticket-number'>#" + currentTicketNumber + "</p>" +
                             "<div class='ticket-body'>" +
                                 "<h2 class='ticket-item-name'>" + item.name + "</h2>" +
@@ -78,9 +79,33 @@
                 $('.ticket-sheet-wrapper').append(htmlString);
             })
             main.ticketsPrinted = true;
+            if (main.printCount) main.makeCountSheet();
+        }
+        this.makeCountSheet = function () {
+            main.items.forEach(function (item) {
+                var htmlString = '', i;
+                for (i = 0; i < item.amount; i++) {
+                    var currentTicketNumber = i + 1;
+                    if (currentTicketNumber === 1) {
+                        htmlString += "<div class='count-item'>" +
+                            "<h3>" + item.name + "<span>" + item.amount + "</span></h3>" +
+                            "<div class='count-col'><div class='count-block'></div>";
+                    } else if (currentTicketNumber % 5 === 0) {
+                        htmlString += "<div class='count-block'></div></div><div class='count-col'>";
+                    } else if (currentTicketNumber === item.amount) {
+                        htmlString += "<div class='count-block'></div></div>";
+                    }else {
+                        htmlString += "<div class='count-block'></div>";
+                    }
+
+                }
+                $('.count-sheet-wrapper').append(htmlString);
+            })
+            main.countPrinted = true;
         }
         this.return = function () {
             main.ticketsPrinted = false;
         }
+        this.printTickets()
     })
 })(angular.module('ticket-generator', []))
